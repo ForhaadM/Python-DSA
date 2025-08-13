90b09502151cfa27066997adb3ffe0cbcd5170d8
ForhaadM <forhaadmiah1@gmail.com>
2025-08-13 11:20:01 -0400

    added Comments

--- Hash Maps/twoSum.py
+++ Hash Maps/twoSum.py
@@ -1,7 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}
-        for i, n in enumerate(nums):
+        # enumerate lets you loop through both the index and the value at the same time.
+        for i, n in enumerate(nums): # i is the index of the current element in nums. # n is the value of the current element in nums. 
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff], i]
